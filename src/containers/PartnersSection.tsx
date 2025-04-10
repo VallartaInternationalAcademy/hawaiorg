32c29c12ff10d01b9d03460c7ee85254f505f66f
coffe67 <coffe67.na@gmail.com>
2025-04-10 11:17:01 -0600

    Adding Partners Link page + Adding New Staff Member

--- src/containers/PartnersSection.tsx
+++ src/containers/PartnersSection.tsx
@@ -4,6 +4,7 @@ export interface partner {
   logo: string;
   name: string;
   description: string;
+  urlLink?: string;
 }
 
 interface PartnersProps {
@@ -14,20 +15,22 @@ const PartnersSection = ({ partners }: PartnersProps) => {
   return (
     <div className="container">
       <div className="row">
-        {partners.map((item, index) => (
-          <div key={index} className="col-lg-4 col-md-6 col-12 mt-4 pt-2">
+        {partners.map((item) => (
+          <div key={item.name} className="col-lg-4 col-md-6 col-12 mt-4 pt-2">
             <div className="d-flex flex-column justify-content-center align-items-center">
               <div className="d-flex justify-content-center align-items-center w-75">
-                <img
-                  src={item.logo}
-                  className="rounded-lg img-fluid max-width:220px"
-                  style={{ maxWidth: "350px", maxHeight: "150px", objectFit: "contain" }}
-                  alt=""
-                />
+                <a href={item.urlLink} target="_blank">
+                  <img
+                    src={item.logo}
+                    className="rounded-lg img-fluid max-width:220px"
+                    style={{ maxWidth: "350px", maxHeight: "150px", objectFit: "contain" }}
+                    alt=""
+                  />
+                </a>
               </div>
               <div className="content ms-3">
                 <h5 className="mb-2 text-center">
-                  <a className="text-dark">{item.name}</a>
+                  <a href={item.urlLink} className="text-dark" target="_blank">{item.name}</a>
                 </h5>
                 <p className="position text-muted" style={{ textAlign: "left" }}>{item.description}</p>
               </div>
