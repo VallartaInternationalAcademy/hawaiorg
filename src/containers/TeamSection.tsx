84a45efa1589f747f61485cc8b8d6f53c3cd0e91
coffe67 <coffe67.na@gmail.com>
2026-04-07 16:57:51 -0600

    Update Images Team About Us Section

--- src/containers/TeamSection.tsx
+++ src/containers/TeamSection.tsx
@@ -16,17 +16,18 @@ const TeamSection = ({ Team }: TeamProps) => {
       <div className="row">
         {Team.map((t, index) => (
           <div key={index} className="col-lg-4 col-md-6 col-12 mt-4 pt-2">
-            <div className="d-flex align-items-center">
+            <div className="d-flex flex-column align-items-center">
               <img
                 src={t.imagen}
-                className="avatar avatar-medium rounded-circle img-thumbnail"
+                className="avatar avatar-large rounded-circle img-fluid"
+                style={{width: '100% \\9'}}
                 alt=""
               />
-              <div className="content ms-3">
+              <div className="content">
                 <h5 className="mb-0">
                   <a className="text-dark">{t.nombre}</a>
                 </h5>
-                <small className="position text-muted">{t.puesto}</small>
+                {/*<small className="position text-muted">{t.puesto}</small>*/}
               </div>
             </div>
           </div>
